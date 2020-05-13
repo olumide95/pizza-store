382bf2d6c3e2b5656e841201f6d72ed5f5f4bdd0
Olumide <olugbemiro.olumide@gmail.com>
2020-05-13 19:59:15 +0100

    handle error for when cart item is empty

--- src/reducers/storeReducer.js
+++ src/reducers/storeReducer.js
@@ -79,10 +79,15 @@ const storeReducer = (state = defaultState, action) => {
     case ADD_TO_CART: {
       let cartItems = state.items.find((item) => item.uuid === action.uuid);
 
-      //check if the action id exists in the cartItems
-      let existed_item = state.cartItems.find(
-        (item) => action.uuid === item.uuid
-      );
+      let existed_item = null;
+
+      if (state.cartItems && state.cartItems.length > 0) {
+        //check if the action id exists in the cartItems
+        let existed_item = state.cartItems.find(
+          (item) => action.uuid === item.uuid
+        );
+      }
+
       if (existed_item) {
         existed_item.quantity += 1;
         localStorage.cartItems = [JSON.stringify(state.cartItems)];
